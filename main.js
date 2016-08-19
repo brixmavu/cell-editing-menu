33e076d1842979504812e1c9da35d6873c64848e
brixton <brixton.mavu@rockectseed.com>
2016-08-19 17:12:10 +0200

    WIP: A generic right click menu

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-$(function () {
-    $("#menu").menu();
+//Trigger action when the contextmenu is about to be shown
+$(".layout_template").on('contextmenu', function (event) {
+    // prevent the default contextmenu
+    event.preventDefault();
+
+    $('.custom-menu').finish().toggle(100).
+
+        //right position (mouse)
+        css({
+            top: event.pageY + "px",
+            left: event.pageX + "px"
+        });
 });
+
+$(".layout_template").mousedown(function (e) {
+    //if the clicked element is not the menu
+    if (!$(e.target).parents(".custom-menu").length > 0) {
+        //hide it
+        $(".custom-menu").hide(100);
+    }
+});
+
+// if the menu element is clicked
+$(".custom-menu li").on('click', function () {
+    // Triggered action name
+    switch ($(this).attr('data-action')) {
+        case 'first':
+            alert('msg');
+            break;
+        case 'second':
+            alert('msg');
+            break;
+        case 'third':
+            alert('msg');
+            break;
+    }
+});
+
+$('.custom-menu').hide(100);
